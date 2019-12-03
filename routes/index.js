3932a802dbef0fb692033c6c0997e70464a37bdd
KewinLizarraga <kewin392@gmail.com>
2019-12-03 03:22:43 -0500

    [UPDATE] post_crear-cuenta and get_iniciar-sesion

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const homeController = require('../controller/homeController');
 const usuariosController = require('../controller/usuariosController');
+const validator = require('../controller/validator');
 
 const router = express.Router();
 
 router.get('/', homeController.home);
 router.get('/crear-cuenta', usuariosController.formCrearCuenta);
+router.post('/crear-cuenta', validator.crearCuenta, usuariosController.crearCuenta);
+router.get('/iniciar-sesion', usuariosController.formIniciarSesion);
 
 module.exports = router;
