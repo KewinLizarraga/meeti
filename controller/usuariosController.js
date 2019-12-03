da83a012b5df46f5b3c2a00650463af6e9a37d4b
KewinLizarraga <kewin392@gmail.com>
2019-12-03 03:20:55 -0500

    [UPDATE] crearCuenta and formIniciarSesion

--- controller/usuariosController.js
+++ controller/usuariosController.js
@@ -1,7 +1,32 @@
+const { check, sanitizeBody, validationResult } = require('express-validator');
+const Usuarios = require('../models/Usuarios');
+
 module.exports = {
   formCrearCuenta: (req, res) => {
     res.render('crear-cuenta', {
       pageName: 'Crear tu cuenta'
+    });
+  },
+  crearCuenta: async (req, res) => {
+    const body = req.body;
+    await check('confirmar').equals(req.body.password).withMessage('El password es diferente').run(req);
+    const errorExpress = validationResult(req);
+    try {
+      await Usuarios.create(body);
+      req.flash('exito', 'Hemos enviado un E-mail, confirma tu cuenta');
+      res.redirect('/iniciar-sesion');
+    } catch (error) {
+      const errorSequelize = error.errors.map(err => err.message);
+      const errExp = errorExpress.errors.map(err => err.msg);
+      const listaErrores = [...errorSequelize, ...errExp];
+
+      req.flash('error', listaErrores);
+      res.redirect('/crear-cuenta');
+    }
+  },
+  formIniciarSesion: (req, res) => {
+    res.render('iniciar-sesion', {
+      pageName: 'Iniciar Sesión'
     });
   }
 }
