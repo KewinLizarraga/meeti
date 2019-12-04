43a45bb34be11b6ca4de86a77ab4c4259e974d71
KewinLizarraga <kewin392@gmail.com>
2019-12-04 00:31:25 -0500

    [UPDATE] crearCuenta and add confirmarCuenta

--- controller/usuariosController.js
+++ controller/usuariosController.js
@@ -1,5 +1,6 @@
 const { check, sanitizeBody, validationResult } = require('express-validator');
 const Usuarios = require('../models/Usuarios');
+const enviarEmail = require('../handlers/email');
 
 module.exports = {
   formCrearCuenta: (req, res) => {
@@ -8,11 +9,19 @@ module.exports = {
     });
   },
   crearCuenta: async (req, res) => {
-    const body = req.body;
+    const usuario = req.body;
     await check('confirmar').equals(req.body.password).withMessage('El password es diferente').run(req);
     const errorExpress = validationResult(req);
     try {
-      await Usuarios.create(body);
+      await Usuarios.create(usuario);
+      const url = `http://${req.headers.host}/confirmar-cuenta/${usuario.email}`;
+      await enviarEmail.enviarEmail({
+        usuario,
+        url,
+        subject: 'Confirmar tu cuenta de Meeti',
+        archivo: 'confirmar-cuenta'
+      });
+
       req.flash('exito', 'Hemos enviado un E-mail, confirma tu cuenta');
       res.redirect('/iniciar-sesion');
     } catch (error) {
@@ -23,6 +32,20 @@ module.exports = {
       req.flash('error', listaErrores);
       res.redirect('/crear-cuenta');
     }
+  },
+  confirmarCuenta: async (req, res, next) => {
+    const usuario = await Usuarios.findOne({ where: { email: req.params.correo } });
+    
+    if (!usuario) {
+      req.flash('error', 'No existe esa cuenta');
+      res.redirect('/crear-cuenta');
+      return next();
+    }
+
+    usuario.activo = 1;
+    await usuario.save();
+    req.flash('exito', 'La cuenta se ha confirmado, ya puedes iniciar sesión');
+    res.redirect('/iniciar-sesion');
   },
   formIniciarSesion: (req, res) => {
     res.render('iniciar-sesion', {
