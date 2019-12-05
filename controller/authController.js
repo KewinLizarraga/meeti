d0a41c47a55d2d3bed562730ad3238825234cc29
KewinLizarraga <kewin392@gmail.com>
2019-12-04 22:26:29 -0500

    [UPDATE] verify if user is authenticated

--- controller/authController.js
+++ controller/authController.js
@@ -6,5 +6,9 @@ module.exports = {
     failureRedirect: '/iniciar-sesion',
     failureFlash: true,
     badRequestMessage: 'Ambos campos son obligatorios'
-  })
+  }),
+  usuarioAuntenticado: (req, res, next) => {
+    if (req.isAuthenticated()) return next();
+    return res.redirect('/iniciar-sesion');
+  }
 }
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const homeController = require('../controller/homeController');
 const usuariosController = require('../controller/usuariosController');
 const authController = require('../controller/authController');
+const adminController = require('../controller/adminController');
 const validator = require('../controller/validator');
 
 const router = express.Router();
@@ -12,5 +13,7 @@ router.post('/crear-cuenta', validator.crearCuenta, usuariosController.crearCuen
 router.get('/confirmar-cuenta/:correo', usuariosController.confirmarCuenta);
 router.get('/iniciar-sesion', usuariosController.formIniciarSesion);
 router.post('/iniciar-sesion', authController.autenticarUsuario);
+
+router.get('/administracion', authController.usuarioAuntenticado, adminController.panelAdministracion);
 
 module.exports = router;
