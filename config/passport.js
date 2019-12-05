57f18f3e7f6a9cae654028ad83ee461ea5233571
KewinLizarraga <kewin392@gmail.com>
2019-12-04 21:27:06 -0500

    [ADD] config passport

---
+++ config/passport.js
@@ -0,0 +1,28 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Usuarios = require('../models/Usuarios');
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+}, async (email, password, next) => {
+  const usuario = await Usuarios.findOne({ where: { email, activo: 1 } });
+
+  if (!usuario) return next(null, false, { message: 'Ese usuario no existe' });
+
+  const verificarPass = usuario.verificarPassword(password);
+
+  if (!verificarPass) return next(null, false, { message: 'Password incorrecto' });
+
+  return next(null, usuario);
+}));
+
+passport.serializeUser(function (usuario, cb) {
+  cb(null, usuario);
+});
+
+passport.deserializeUser(function (usuario, cb) {
+  cb(null, usuario);
+});
+
+module.exports = passport;
